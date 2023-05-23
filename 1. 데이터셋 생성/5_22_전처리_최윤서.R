a9fb1abf015a6576b2b5dff8c3b3b2dc9a9adf3c
garlicbear <78363129+garlicbear@users.noreply.github.com>
2023-05-23 22:45:41 +0900

    Update 5_22_전처리_최윤서.R
    5/23 감정변수 추가 완료

--- 1. 데이터셋 생성/5_22_전처리_최윤서.R
+++ 1. 데이터셋 생성/5_22_전처리_최윤서.R
@@ -1,7 +1,7 @@
 library(dplyr)
 setwd("C:/CYS/의생명통계학/기말과제")
 df<-read.csv("final_df.csv",header=T)
-df<-final_df
+
 colnames(df)
 #1. age가 청소년인 것만 추출
 df1<-df[df$AGE2<=6,] #총 68263건
@@ -11,9 +11,11 @@ df1<-df[df$AGE2<=6,] #총 68263건
 df2<-df1[(df1$YETCGJOB<5)&(df1$YEPGDJOB<5),] #총 64433건. (개수 별로 안 줆.)
 ##두 변수를 더하기
 df2$goodjob<-df2$YETCGJOB+df2$YEPGDJOB
+table(df2$goodjob)
+#**2~5는 1, 6~8은 0 바꾸기!
 
 #3. IRSEX 결측치 확인
-unique(df2$IRSEX) #삭제할 값 없음 
+unique(df2$IRSEX) #삭제할 값 없음. 
 
 #4. HALLYRLST 결측치 처리
 df4<-df2%>%filter(HALLYRLST %in% c(2013,2014,2015,2016,2017,2018,2019,9991))
@@ -31,19 +33,21 @@ sort(unique(df7$WRKDHRSWK2)) #정상적인 값 중 최대치는 61. 999도 살�
 df9<-df7%>%filter(WRKDHRSWK2 %in% c(1:62,999))
 unique(df9$WRKDHRSWK2) #제대로 필터링됨.
 
-#10. WRKSTATWK2
-sort(unique(df9$WRKSTATWK2)) 
-df10<-df9
-df10$WRKSTATWK2_new<-replace(df9$WRKSTATWK2, df9$WRKSTATWK2>=3,0)
-unique(df10$WRKSTATWK2_new) #완료. 
+#10. WRKSTATWK2-삭제. 
+#sort(unique(df9$WRKSTATWK2)) 
+#table(df9$WRKSTATWK2)
+#df10<-df9
+#df10$WRKSTATWK2_new<-replace(df9$WRKSTATWK2, df9$WRKSTATWK2>=3,0)
+#unique(df10$WRKSTATWK2_new) #완료. 
 
-#11. EDUSKPMON
-sort(unique(df10$EDUSKPMON)) #don't know까지는...남겨둬야하지 않을지...일단은 삭제. 
-df11<-df10%>%filter(EDUSKPMON %in% c(0:31,99)) 
-sort(unique(df10$EDUSKPMON)) 
+#11. EDUSKPMON-삭제. (legitimate skip이 해결되지 않음. )
+#sort(unique(df10$EDUSKPMON)) #don't know까지는...남겨둬야하지 않을지...일단은 삭제. 
+#table(df9$EDUSKPMON)
+#df11<-df9%>%filter(EDUSKPMON %in% c(0:31,99)) 
+#sort(unique(df10$EDUSKPMON)) 
 
 #12. MOVSINPYR2
-df12<-df11%>%filter(MOVSINPYR2 %in% c(0,1,2,3)) 
+df12<-df9%>%filter(MOVSINPYR2 %in% c(0,1,2,3)) 
 sort(unique(df12$MOVSINPYR2))  #완료
 
 #13. EDUSCHGRD2-->삭제해야할 것. 왜 있지?? 
@@ -55,6 +59,8 @@ unique(df13$YOSEEDOC)
 df14<-df13%>%filter((YEYFGTSW %in% c(1,2,3,4,5))&(YEYSTOLE %in% c(1,2,3,4,5))) #l.s 제외해도 ok. 
 unique(df14$YEYFGTSW)
 unique(df14$YEYSTOLE)
+###**두 범주로 나눠서, 두 변수 모두 유지하기. (한번도 안 해본 그룹과, 나머지로.)
+
 #15. HPDRGTALK 
 df15<-df14%>%filter(HPDRGTALK %in% c(1,2,91,93))#l.s 제외해도 ok. 
 unique(df15$HPDRGTALK)
@@ -62,7 +68,14 @@ unique(df15$HPDRGTALK)
 #df16<-df15%>%filter(SNRLGSVC %in% c(1,2,3,4,5,6))#값이 없다...없애야 할듯 흑흑. 
 #unique(df15$SNRLGSVC)
 #17. PRXYDATA
-df17<-df15%>%filter(PRXYDATA %in% c(2))
+df17<-df15%>%filter(PRXYDATA %in% c(1))
 table(df15$PRXYDATA) #무시..할까..? 아니면 yes만 남길까?
+#18. 감정변수들
+#a<-df17%>%filter((YODPREV %in% c(1,2))&(YOWRHRS %in% c(1,2,3,4))&(YOLOSEV %in% c(1,2))&(YODSCEV %in% c(1,2)))
+table(df17$YODPREV)
+df18<-df17%>%filter(YODPREV %in% c(1,2))
+table(df17$YOWRHRS)
+table(df17$YOLOSEV)
+table(df17$YODSCEV)
 
-write.csv(df17,"final_df2.csv")
+write.csv(df18,"final_df3.csv")
